cdd010267a70458b8f0792295dea786de2f6db9d
Denison Kolling <denisonkolling@gmail.com>
2024-10-03 16:43:02 -0300

    feat(reviews): implement get reviews by place ID

--- apps/backend/src/app/review/review.controller.ts
+++ apps/backend/src/app/review/review.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ReviewService } from './review.service';
 
-@Controller('review')
+@Controller('reviews')
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {}
+  constructor(private readonly reviewService: ReviewService) { }
+
+  @Get()
+  async getReviews(@Query('placeId') placeId: string) {
+    return await this.reviewService.getReviews(placeId);
+  }
 }
