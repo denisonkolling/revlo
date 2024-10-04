21ce66757b3fa67cb76579f0d34b56218372f25d
Denison Kolling <denisonkolling@gmail.com>
2024-10-04 15:51:20 -0300

    build: update mikro orm entities discovery path settings

--- apps/backend/src/config/mikro-orm.config.ts
+++ apps/backend/src/config/mikro-orm.config.ts
@@ -1,6 +1,8 @@
 import { Options, PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { Migrator } from '@mikro-orm/migrations';
 import * as dotenv from 'dotenv';
+import { Review } from '../app/review/entities/review.entity';
+import { User } from '../app/user/entities/user.entity';
 
 dotenv.config();
 
@@ -11,8 +13,18 @@ const mikroOrmConfig: Options = {
     driver: PostgreSqlDriver,
     dbName: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    entities: ['./dist/app/**/*.entity.js'],
-    entitiesTs: ['./src/app/**/*.entity.ts'],
+    entities: [User, Review],
+    entitiesTs: [User, Review],
+
+    /** TODO
+    * - Check and adjust the relative path for entities, according to the environment:
+    * - Use 'entities' with the.js file path for the production environment(after transpilation).
+    * -  Use 'entitiesTs' with the.ts file path for the development environment(before transpilation).
+    */
+
+    // entities: ['./dist/app/**/*.entity.js']
+    // entitiesTs: ['./src/app/**/*.entity.ts']
+
     extensions: [
         Migrator,
     ],
@@ -22,5 +34,7 @@ const mikroOrmConfig: Options = {
     },
 
 };
+
+
 
 export default mikroOrmConfig;
