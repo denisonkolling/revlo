52d29cca1ed529c954e20888a7cc606b2fc06ad5
Denison Kolling <denisonkolling@gmail.com>
2024-10-04 11:20:41 -0300

    build: configure MikroORM entity paths and settings

--- apps/backend/src/config/mikro-orm.config.ts
+++ apps/backend/src/config/mikro-orm.config.ts
@@ -1,19 +1,23 @@
 import { Options, PostgreSqlDriver } from '@mikro-orm/postgresql';
-import { Review } from '../app/review/entities/review.entity';
+import { Migrator } from '@mikro-orm/migrations';
 
 const mikroOrmConfig: Options = {
     port: parseInt(process.env.DB_PORT, 10) || 5432,
-    user: process.env.DB_USERNAME,
-    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
     driver: PostgreSqlDriver,
-    dbName: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    entities: [Review],
-    entitiesTs: [Review],
+    dbName: process.env.DB_NAM || 'revlo_v2',
+    password: process.env.DB_PASSWORD || '102030',
+    entities: ['./dist/app/**/*.entity.js'],
+    entitiesTs: ['./src/app/**/*.entity.ts'],
+    extensions: [
+        Migrator,
+    ],
     migrations: {
-        path: 'dist/migrations',
-        pathTs: 'src/migrations',
+        path: './dist/migrations',
+        pathTs: './src/migrations',
     },
+
 };
 
 export default mikroOrmConfig;
