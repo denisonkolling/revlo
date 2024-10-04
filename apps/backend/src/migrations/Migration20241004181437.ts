60bed32f5959ee712b01cf038e9f90774b73e919
Denison Kolling <denisonkolling@gmail.com>
2024-10-04 15:50:47 -0300

    feat(user): add create user endpoint with email uniqueness validation and error handling

---
+++ apps/backend/src/migrations/Migration20241004181437.ts
@@ -0,0 +1,14 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20241004181437 extends Migration {
+
+  override async up(): Promise<void> {
+    this.addSql(`drop table if exists "tab_users" cascade;`);
+  }
+
+  override async down(): Promise<void> {
+    this.addSql(`create table "tab_users" ("id" serial primary key, "email" varchar(255) not null, "password" varchar(255) not null, "created_at" timestamptz not null, "updated_at" timestamptz not null, "deleted_at" timestamptz null, "account_status" varchar(255) not null);`);
+    this.addSql(`alter table "tab_users" add constraint "tab_users_email_unique" unique ("email");`);
+  }
+
+}
