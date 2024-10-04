b1fad6df37300b7cc1dfcd09bcf8e7db112b41e9
Denison Kolling <denisonkolling@gmail.com>
2024-10-04 11:41:45 -0300

    build(dotenv): add dotenv dependency for environment variable management at mikro orm

--- apps/backend/src/config/mikro-orm.config.ts
+++ apps/backend/src/config/mikro-orm.config.ts
@@ -1,13 +1,16 @@
 import { Options, PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { Migrator } from '@mikro-orm/migrations';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const mikroOrmConfig: Options = {
     port: parseInt(process.env.DB_PORT, 10) || 5432,
-    user: process.env.DB_USERNAME || 'postgres',
-    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST,
     driver: PostgreSqlDriver,
-    dbName: process.env.DB_NAM || 'revlo_v2',
-    password: process.env.DB_PASSWORD || '102030',
+    dbName: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
     entities: ['./dist/app/**/*.entity.js'],
     entitiesTs: ['./src/app/**/*.entity.ts'],
     extensions: [
